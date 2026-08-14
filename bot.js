5f060504e3ea13db586c56f4fcd26065fdd26390
NILOYGHOSH88 <n7753522@gmail.com>
2026-08-14 13:33:37 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,4 +9,17 @@ const bot = mineflayer.createBot({
 
 bot.on('spawn', () => {
   console.log('বট সফলভাবে সার্ভারে ঢুকেছে!')
+  
+  // প্রতি ৫ সেকেন্ড পর পর বট ডানে বা বামે একটু ঘুরবে এবং লাফাবে
+  setInterval(() => {
+    bot.look(Math.random() * Math.PI * 2, 0)
+    bot.setControlState('jump', true)
+    setTimeout(() => {
+      bot.setControlState('jump', false)
+    }, 500)
+  }, 5000)
+})
+
+bot.on('end', () => {
+  console.log('ডিসকানেক্ট হয়ে গেছে...')
 })
